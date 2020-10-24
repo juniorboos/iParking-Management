e6cc37e3b3b92ef3e534596a5235d4e635f063db
Milton Boos Junior <junior_boos@live.com>
2020-10-24 04:08:49 +0100

    communicate auth with api

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,58 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import {FiLogIn} from 'react-icons/fi';
 // import {Link, useHistory} from 'react-router-dom'
-// import api from '../../services/api'
+import api from '../../services/api'
 // import firebase from '../../services/firebase'
 
 import './styles.css';
 
-export default function Dashboard() {
+export default function Login() {
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
 
-   const login = () => {
-      // if (user == "" || pass == "") {
-      //    return alert("Error, empty field!");
-      // }
-
-      // firebase
-      //    .auth()
-      //    .signInWithEmailAndPassword(user, pass)
-      //    .then(() => {
-      //       console.log("Autenticado - Enviando para rota correta...");
-      //       navigation.reset({
-      //          index: 0,
-      //          routes: [{ name: "RoutesDrawer" }],
-      //       })
-      //       // navigation.dispatch(
-      //       //    CommonActions.reset({
-      //       //       index: 0,
-      //       //       routes: [{ name: "RoutesDrawer" }],
-      //       //    })
-      //       // );
-      //    })
-      //    .catch((error) => {
-      //       createButtonAlert("Error", error.toString())
-      //       if (
-      //          error == "auth/wrong-password" ||
-      //          error ==
-      //          "The password is invalid or the user does not have a password."
-      //       ) {
-      //          return createButtonAlert("Error", "Wrong Password!");
-      //       }
-      //       if (error == "auth/invalid-email") {
-      //          return createButtonAlert("Error", "Invalid Email!");
-      //       }
-      //       console.log(error);
-      //       // alert(error);
-      //    });
+   function login (e) {
+      e.preventDefault()
+      const data = { email, password }
+      try {
+         api.post('authenticate', data)
+            .then((response) => {
+               console.log(response)
+            })
+      } catch (err) {
+         console.log(err)
+         alert('An error ocurred, try again.')
+      }
    }
 
    return (
       <div className="loginContainer">
-         <form className="box" action="index.html" method="post">
+         <form className="box" onSubmit={login}>
             <h1>Login</h1>
-            <input type="text" name="" placeholder="Username"/>
-            <input type="password" name="" placeholder="Password"/>
-            <input type="submit" name="" value="Login"/>
+            <input 
+               type="text" 
+               name="" 
+               placeholder="Username"
+               onChange={e => setEmail(e.target.value)}  
+            />
+            <input 
+               type="password"
+               name="" 
+               placeholder="Password"
+               onChange={e => setPassword(e.target.value)}
+            />
+            <button 
+               type="submit" 
+               name="" 
+            >Login</button>
          </form>
       </div>
       
