abac193192acf1b55ff740d03ab217fd555466f2
Milton Boos Junior <junior_boos@live.com>
2020-10-25 01:21:59 +0000

    persist session after refresh

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import './global.css';
 import Routes from './routes'
+import { setAccessToken } from './services/accessToken';
+import api from './services/api';
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    api.post("refresh_token", {}, { withCredentials: true })
+      .then( response => {
+        setAccessToken(response.data.accessToken)
+        setLoading(false)
+      })
+  }, [])
+
+  if(loading) {
+    return <div>loading...</div>
+  }
   return (
     <Routes/>
   );
