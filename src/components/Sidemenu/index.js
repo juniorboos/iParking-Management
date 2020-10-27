863b442329aaefd50a845b665674ea17d0cbcee7
Milton Boos Junior <junior_boos@live.com>
2020-10-27 00:51:18 +0000

    added logout button and profile style

--- src/components/Sidemenu/index.js
+++ src/components/Sidemenu/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
+import * as CgIcons from 'react-icons/cg';
 import { Link, useHistory } from 'react-router-dom';
 import { SidemenuData } from './SidemenuData';
 import { IconContext } from 'react-icons';
@@ -36,24 +37,7 @@ export default function Sidemenu(props) {
    return (
       <>
          <IconContext.Provider value={{color:'#fff'}}>
-            {/* <div className="navbar">
-               <Link to='#' className='menu-bars' >
-                  <FaIcons.FaBars onClick={showSidemenu} />
-               </Link>
-               <ul className="nav-menu-items" onClick={showSidemenu}>
-                  {SidemenuData.map((item, index) => {
-                     return (
-                        <li key={index} className={item.cName}>
-                           <Link to={item.path}>
-                              {item.icon}
-                           </Link>
-                        </li>
-                     )
-                  })}
-               </ul>
-            </div> */}
             <nav style={!props.show ? {display: 'none'} : null} className={sidemenu ? 'nav-menu active' : 'nav-menu'}>
-            {/* <nav className={'nav-menu'}> */}
                <Link to='#' className="menu-bars" onClick={showSidemenu}>
                   {sidemenu ? (
                      <FaIcons.FaChevronLeft  />
@@ -63,14 +47,23 @@ export default function Sidemenu(props) {
                   
                </Link>
                <ul className="nav-menu-items">
-                  <li>{user.name}</li>
-                  <li>{user.email}</li>
+                  <li className={sidemenu ? "profile active" : "profile"}> 
+                     <div className="profileIcon">
+                        <FaIcons.FaUserCircle size={18}/>
+                     </div>
+                     
+                     <div className={sidemenu ? 'contents active' : 'contents'}>
+                        <div className={sidemenu ? 'inner active' : 'inner'}>
+                           {user.name.split(" ")[0]}
+                        </div>
+                     </div>   
+                  </li>            
+                     
                   {SidemenuData.map((item, index) => {
                      return (
                         <li key={index} className={sidemenu ? 'nav-text active' : 'nav-text'}>
                            <Link to={item.path}>
                               {item.icon}
-                              {/* <span className={sidemenu ? 'title active': 'title'}>{item.title}</span>                               */}
                               <div className={sidemenu ? 'contents active' : 'contents'}>
                                  <div className={sidemenu ? 'inner active' : 'inner'}>
                                     {item.title}
@@ -80,7 +73,18 @@ export default function Sidemenu(props) {
                         </li>
                      )
                   })}
-                  <button type="button" onClick={logout}>LOGOUT</button>
+
+                  <button type="button" onClick={logout} className={sidemenu ? 'logout-button active' : 'logout-button'}>
+                        <div className="profileIcon">
+                           <CgIcons.CgLogOut size={18}/>
+                        </div>
+                        <div className={sidemenu ? 'contents active' : 'contents'}>
+                           <div className={sidemenu ? 'inner active' : 'inner'}>
+                              Logout
+                           </div>
+                        </div>   
+                  </button>
+                  {/* <button type="button" onClick={logout}>LOGOUT</button> */}
                </ul>
             </nav>
          </IconContext.Provider>
