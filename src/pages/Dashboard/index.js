4bfd8c1f9b8f2a4c796f7468fd0e1983b654a206
Milton Boos Junior <junior_boos@live.com>
2020-10-27 23:55:54 +0000

    stylized dashboard grid

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import {FiLogIn} from 'react-icons/fi';
 // import {Link, useHistory} from 'react-router-dom'
-// import api from '../../services/api'
+import Select from 'react-select';
+import api from '../../services/api'
 
 import './styles.css';
 
 export default function Dashboard() {
+   const [parkings, setParkings] = useState([])
+   const [parking, setParking] = useState()
+
+   const customStyles = {
+      control: base => ({
+        ...base,
+        height: 48,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'rgb(160, 160, 160)',
+        fontSize: 18
+      })
+   };
 
    return (
-      <div className="wrapper">
-         Dashboard
+      <div className="dashboard-wrapper">
+         <Select
+            styles={customStyles}
+            label="Parking"
+            className="select"
+            onChange={selectedOption => setParking(selectedOption)}
+            options={parkings}
+            isSearchable
+            placeholder="Select parking"
+         />
+         <div className="cards-wrapper">
+            <div className="upper-cards">
+               <div className="card"></div>
+               <div className="card"></div>
+            </div>
+            <div className="lower-cards">
+               <div className="card"></div>
+               <div className="card"></div>
+               <div className="card"></div>
+            </div>
+            
+         </div>
       </div>
    );
 };
